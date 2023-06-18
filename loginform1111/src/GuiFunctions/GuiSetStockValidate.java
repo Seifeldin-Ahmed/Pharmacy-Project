d15c75e5e3a16bd69bd7f4b62f12861de0c71a02
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2023-06-19 01:59:03 +0200

    3 function from set_stock

---
+++ loginform1111/src/GuiFunctions/GuiSetStockValidate.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GuiFunctions;
+
+import Modeling.Products_Model;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author lenovo
+ */
+public class GuiSetStockValidate {
+    
+      public String DatePattern = "^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)(?:0?[13-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$";
+        
+        public boolean SetStock_validateFields(String prod_name, String pri, String quan, String ex_date)
+        {
+        String med_name=prod_name;
+        String price = pri;
+        String quantity = quan;
+        String expired_date = ex_date;
+        return   !expired_date.equals("") && !quantity.equals("") && !price.equals("") && !med_name.equals("");
+        }
+        
+        public boolean vaildate_ExpiredDate(Products_Model old_Product,Products_Model new_Product)
+        {
+        boolean vaild=false;
+        String old_date = old_Product.getExpired_date();
+         String new_date = new_Product.getExpired_date();
+        StringTokenizer old = new StringTokenizer(old_date, "/");
+        StringTokenizer New = new StringTokenizer(new_date, "/");
+        int DayOld = Integer.parseInt(old.nextToken());
+        int MonthOld = Integer.parseInt(old.nextToken());
+        int YearOld = Integer.parseInt(old.nextToken());    
+        int DayNew = Integer.parseInt(New.nextToken());
+        int MonthNew = Integer.parseInt(New.nextToken());
+        int YearNew = Integer.parseInt(New.nextToken());
+        if (YearNew > YearOld) // Expired
+        {
+            vaild = true;
+        } 
+        else if (YearNew == YearOld) {
+            if (MonthNew > MonthOld) {
+                vaild = true;
+            } 
+            else if (MonthNew == MonthOld) {
+                if (DayNew >= DayOld) {
+                    vaild = true;
+                }
+            }
+        }
+        return vaild;
+    }
+        
+      public boolean IsExpired(Products_Model Product) {
+        boolean expired = false;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        String old_date = Product.getExpired_date();
+        StringTokenizer old = new StringTokenizer(old_date, "/");
+        StringTokenizer Now = new StringTokenizer(dtf.format(now), "/");
+        int DayOld = Integer.parseInt(old.nextToken());
+        int MonthOld = Integer.parseInt(old.nextToken());
+        int YearOld = Integer.parseInt(old.nextToken());
+        int YearNow = Integer.parseInt(Now.nextToken());
+        int MonthNow = Integer.parseInt(Now.nextToken());
+        int DayNow = Integer.parseInt(Now.nextToken());
+        if (YearNow > YearOld) // Expired
+        {
+            expired = true;
+        } else if (YearNow == YearOld) {
+            if (MonthNow > MonthOld) {
+                expired = true;
+            } else if (MonthNow == MonthOld) {
+                if (DayNow >= DayOld) {
+                    expired = true;
+                }
+            }
+        }
+        return expired;
+    }
+      
+      
+}
+
+
---
+++ loginform1111/test/GuiFunctions/GuiSetStockValidateTest.java
@@ -0,0 +1,126 @@
+package GuiFunctions;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import GuiFunctions.GuiSetStockValidate;
+import Modeling.Products_Model;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author lenovo
+ */
+public class GuiSetStockValidateTest {
+    
+    public GuiSetStockValidateTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of SetStock_validateFields method, of class GuiSetStockValidate.
+     */
+    @Test
+    public void testSetStock_validateFields() {
+         System.out.println("Empty fields");
+        String prod_name = "";
+        String pri = "";
+        String quan = "";
+        String ex_date = "";
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = false;
+        boolean result = instance.SetStock_validateFields(prod_name, pri, quan, ex_date);
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testSetStock2_validateFields() {
+         System.out.println("Empty fields");
+        String prod_name = "alva";
+        String pri = "50";
+        String quan = "";
+        String ex_date = "";
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = false;
+        boolean result = instance.SetStock_validateFields(prod_name, pri, quan, ex_date);
+        assertEquals(expResult, result);
+    }
+     @Test
+    public void testSetStock3_validateFields() {
+         System.out.println("not Empty fields");
+        String prod_name = "alvantern";
+        String pri = "70";
+        String quan = "50";
+        String ex_date = "20/12/2026";
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = true;
+        boolean result = instance.SetStock_validateFields(prod_name, pri, quan, ex_date);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testVaildate_ExpiredDate() {
+        System.out.println("vaildate_ExpiredDate");
+        Products_Model old_product =new Products_Model ("move", 70,"1/8/2010",202,"Other","hh"); 
+        Products_Model new_product =new Products_Model ("move", 70,"1/8/2020",202,"Other","hh"); 
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = true;
+        boolean result = instance.vaildate_ExpiredDate(old_product,new_product);
+        assertEquals(expResult, result);
+    }
+        @Test
+    public void testVaildate_ExpiredDate2() {
+        System.out.println("vaildate_ExpiredDate");
+        Products_Model old_product =new Products_Model ("move", 70,"1/8/2020",202,"Other","hh"); 
+        Products_Model new_product =new Products_Model ("move", 70,"1/8/2010",202,"Other","hh"); 
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = false;
+        boolean result = instance.vaildate_ExpiredDate(old_product,new_product);
+        assertEquals(expResult, result);
+      
+    }
+    @Test
+    public void testIsExpired() {
+        System.out.println("IsnotExpired");
+        Products_Model product =new Products_Model ("move", 70,"1/8/2030",202,"Other","hh"); 
+       // Products_Model Product = null;
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = false;
+        boolean result = instance.IsExpired(product);
+        assertEquals(expResult, result);
+       
+    }
+     @Test
+    public void testIsExpired2() {
+        System.out.println("IsExpired");
+        Products_Model product =new Products_Model ("move", 70,"1/8/2022",202,"Other","hh"); 
+       // Products_Model Product = null;
+        GuiSetStockValidate instance = new GuiSetStockValidate();
+        boolean expResult = true;
+        boolean result = instance.IsExpired(product);
+        assertEquals(expResult, result);
+    }
+   
+}
